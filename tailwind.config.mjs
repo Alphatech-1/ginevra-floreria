c74fda904039ba9076c979fb76bdd133caadae1a
Alphatech-1 <alfaropanayfocristhian@gmail.com>
2026-01-24 00:04:56 -0500

    Ginevra web

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#F2E5C6',
+        primary: '#75162D',
+        secondary: '#560B18',
+        dark: '#3B010B',
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
