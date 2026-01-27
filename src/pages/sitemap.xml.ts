23c20845faf19c3b79893418cb169af55769848a
Cristhian Alfaro <alfaropanayfocristhian@gmail.com>
2026-01-27 12:21:15 -0500

    Update sitemap.xml.ts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,28 +1,31 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = () => {
+export async function GET() {
   const pages = [
-    '/',
-    '/#nosotros',
-    '/#catalogo',
-    '/#preguntas',
-    '/#contacto',
+    "",
+    "nosotros",
+    "galeria",
+    "preguntas",
+    "contacto"
   ];
 
-  const urls = pages.map((page) => `
-    <url>
-      <loc>https://ginevraperu.com${page}</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.9</priority>
-    </url>
-  `).join('');
+  const urls = pages
+    .map(
+      (page) => `
+  <url>
+    <loc>https://ginevraperu.com/${page}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>${page === "" ? "1.0" : "0.8"}</priority>
+  </url>`
+    )
+    .join("");
 
-  return new Response(`
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${urls}
-    </urlset>
-  `, {
-    headers: { 'Content-Type': 'application/xml' }
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+  return new Response(sitemap.trim(), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
   });
-};
+}
